d42807664fe05e6613c25e6d941054e59a9e1027
Eugen Sunic <eugen.sunic@extern.consorsbank.de>
2018-05-20 17:49:40 +0200

    added research content along with 3 extra images

--- js/components/researchcomponent/Contentobject.js
+++ js/components/researchcomponent/Contentobject.js
@@ -1,4 +1,11 @@
 export const content = {
+  date0: 'May 14, 2018',
+  url0: 'https://www.ncbi.nlm.nih.gov/pubmed/29761234',
+  showpdf0: false,
+  heading0: 'Prevalence and predictors of SIBO',
+  image0: '../../pics/organs/colon.png',
+  info_content0: `The reported prevalence of small intestinal bacterial overgrowth (SIBO) among individuals with irritable bowel syndrome (IBS) is highly variable. The aim of the study is to estimate the prevalence and identify predictors of SIBO in IBS.`,
+
   date1: 'March 05, 2018',
   url1: 'https://www.medscape.com/viewarticle/893399',
   showpdf: false,
@@ -52,30 +59,82 @@ He also made a general observation about PPI-related research: "Most studies sho
   conclusion2: `<p>Long-term use of PPIs was still associated with an increased GC risk in subjects even after HP eradication therapy.</p>`,
 
   // --------------------------------------------------------------------------------
-
-  date3: 'July 25, 2017',
-  url3: 'https://www.medscape.com/viewarticle/883357',
+  date3: 'September 21, 2017',
+  url3: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5643276/',
   showpdf3: false,
-  heading3: 'PPIs Not Linked to Cognitive Decline',
-  image3: '../../pics/organs/stomach.png',
-  info_content3: ` Proton pump inhibitor (PPI) use was not associated with an increased risk for cognitive decline in an analysis of data from the Nurses' Health Study II.
+  heading3: 'Proton pump inhibitors therapy and risk of Clostridium difficile infection',
+  image3: '../../pics/organs/colon.png',
+  info_content3: `Fifty-six studies (40 case-control and 16 cohort) involving 356683 patients met the inclusion criteria and were analyzed. Both the overall pooled estimates and subgroup analyses showed increased risk for CDI despite substantial statistical heterogeneity among studies`,
+
+  // --------------------------------------------------------------------------------
+  // --------------------------------------------------------------------------------
+
+  date4: 'July 25, 2017',
+  url4: 'https://www.medscape.com/viewarticle/883357',
+  showpdf4: false,
+  heading4: 'PPIs Not Linked to Cognitive Decline',
+  image4: '../../pics/organs/brain.png',
+  info_content4: ` Proton pump inhibitor (PPI) use was not associated with an increased risk for cognitive decline in an analysis of data from the Nurses' Health Study II.
   Although the results provide reassuring data from a large prospective study, they are unlikely to completely resolve safety concerns about long-term use of PPIs.
   Paul Lochhead, PhD, from Massachusetts General Hospital and Harvard Medical School, Boston, and colleagues published their findings online July 20 in Gastroenterology.`,
 
-  stats3: `
+  stats4: `
      <p> - among 63 397 eligible subjects, 153 (0.24%) developed gastric cancer during a median follow-up of 7.6 years </p>
      <p> - PPIs use was associated with an increased GC risk (HR 2.44, 95% CI 1.42 to 4.20), while H2RA was not (HR 0.72, 95% CI 0.48 to 1.07) </p>
      <p> - risk increased with duration of PPIs use (HR 5.04, 95% CI 1.23 to 20.61; 6.65, 95% CI 1.62 to 27.26 and 8.34, 95% CI 2.02 to 34.41 for ≥1 year, ≥2 years and ≥3 years, respectively)</p>
      <p> - The adjusted absolute risk difference for PPIs VS non-PPIs use was 4.29 excess GC (95% CI 1.25 to 9.54) per 10 000 person-years</p>`,
 
-  problem3: `<p>The new results also conflict with a recently published, US Food and Drug Administration–mandated follow-up study conducted with pantoprazole, said David A. Johnson, MD, chief of gastroenterology
+  problem4: `<p>The new results also conflict with a recently published, US Food and Drug Administration–mandated follow-up study conducted with pantoprazole, said David A. Johnson, MD, chief of gastroenterology
   at Eastern Virginia Medical School in Norfolk (Aliment Pharmacol Ther. 2016;43:73-82).</p>
-<p>"No increased risk [for gastric cancer] was observed with prolonged PPI exposure," he said.</p>
-<p>Dr Johnson, who was asked for comment, also stated that the study has a "geographic bias" because it is from Hong Kong and "specific risks for gastric cancer are well recognized in Asian patients."</p>
-<p>In terms of study weaknesses, the researchers  lacked information on some risk factors, such as diet, family history, and socioeconomic status.  And despite the large sample of more than 63,000 H pylori–infected patients,
- the small number of gastric cancer cases did not allow for any "meaningful evaluation of the dosage effect and role of different PPIs," the researchers say. </p>
- <p>Dr Johnson said there was another study weakness: "Important" demographic variables that are risk factors for gastric cancer — such as  smoking, alcohol use, obesity, diet, and family history — are not accounted for.
-He also made a general observation about PPI-related research: "Most studies showed that any potential effects of PPIs tend to disappear with time and that the most likely explanation for the effects is confounding by indication rather than causality."</p>`,
+  <p>"No increased risk [for gastric cancer] was observed with prolonged PPI exposure," he said.</p>
+  <p>Dr Johnson, who was asked for comment, also stated that the study has a "geographic bias" because it is from Hong Kong and "specific risks for gastric cancer are well recognized in Asian patients."</p>
+  <p>In terms of study weaknesses, the researchers  lacked information on some risk factors, such as diet, family history, and socioeconomic status.  And despite the large sample of more than 63,000 H pylori–infected patients,
+  the small number of gastric cancer cases did not allow for any "meaningful evaluation of the dosage effect and role of different PPIs," the researchers say. </p>
+  <p>Dr Johnson said there was another study weakness: "Important" demographic variables that are risk factors for gastric cancer — such as  smoking, alcohol use, obesity, diet, and family history — are not accounted for.
+  He also made a general observation about PPI-related research: "Most studies showed that any potential effects of PPIs tend to disappear with time and that the most likely explanation for the effects is confounding by indication rather than causality."</p>`,
 
-  conclusion3: `<p>Long-term use of PPIs was still associated with an increased GC risk in subjects even after HP eradication therapy.</p>`
+  conclusion4: `<p>Long-term use of PPIs was still associated with an increased GC risk in subjects even after HP eradication therapy.</p>`,
+
+  date5: 'July 04, 2017',
+  url5: 'https://www.ncbi.nlm.nih.gov/pubmedhealth/behindtheheadlines/news/2017-07-04-heartburn-drugs-linked-to-premature-death/',
+  showpdf5: false,
+  heading5: 'Heartburn drugs linked to premature death',
+  image5: '../../pics/organs/stomach.png',
+  info_content5: `Medications used to treat acid reflux/heartburn problems have been linked to an elevatd risk of premature death in a new study.
+A team of US researchers conducted an observational study on data from the US Department of Veterans Affairs. Compared with H2 blocker such as ranitidine, cimetidine use, PPI use was associated with a 25% higher relative risk of death from all causes.`,
+
+  date6: 'April 24, 2017',
+  url6: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5411168/',
+  showpdf6: false,
+  heading6: 'Proton-pump inhibitors use, and risk of acute kidney injury',
+  image6: '../../pics/organs/kidney.jpg',
+  info_content6: `Recent studies have suggested a potential increased risk of acute kidney injury (AKI) among proton-pump inhibitor (PPI) users. However, the present results are conflicting. Thus, we performed a meta-analysis to investigate the association between PPI therapy and the risk of AKI.`,
+
+  date7: 'February 01, 2016',
+  url7: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4772730/',
+  showpdf7: false,
+  heading7: 'Proton Pump Inhibitor Use and Risk of Chronic Kidney Disease',
+  image7: '../../pics/organs/kidney.jpg',
+  info_content7: `Proton pump inhibitors (PPIs) are among the most commonly used drugs worldwide, and have been linked to acute interstitial nephritis. Less is known about the relationship between PPI use and chronic kidney disease (CKD).`,
+
+  date8: 'May 19, 2015',
+  url8: 'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4575851/',
+  showpdf8: false,
+  heading8: 'Proton Pump inhibitors associated with fractures in young adults',
+  image8: '../../pics/organs/bones.png',
+  info_content8: `Proton pump inhibitors (PPIs) are associated with fracture in adults with osteoporosis. Because PPI therapy may interfere with bone accrual and attainment of peak bone mineral density, we studied the association between use of PPIs and fracture in children and young adults.`,
+
+  date9: 'June 24, 2013',
+  url9: 'https://bmcmedresmethodol.biomedcentral.com/articles/10.1186/1471-2288-13-82',
+  showpdf9: false,
+  heading9: 'Proton pump inhibitors and the risk of pneumonia',
+  image9: '../../pics/organs/lungs.png',
+  info_content9: `To compare the results of a new-user cohort study design and the self-controlled case series (SCCS) design using the risk of hospitalisation for pneumonia in those dispensed proton pump inhibitors compared to those unexposed as a case study.`,
+
+  date10: '',
+  url10: '',
+  showpdf10: false,
+  heading10: '',
+  image10: '',
+  info_content10: ``
 };
--- js/components/researchcomponent/Research.js
+++ js/components/researchcomponent/Research.js
@@ -28,40 +28,80 @@ export default class Research extends React.Component {
         <Navbar />
         <Quickstats />
         <p className="title_research">Published research</p>
-
         <div className="row">
           <div className="col-sm-6">
             <div className="research_wrapper">
-              <Headerfirst id="1" date={content.date1} url={content.url1} show_pdf={false} />
+              <Headerfirst id="1" date={content.date0} url={content.url0} show_pdf={false} />
+              <Headermain heading={content.heading0} image={content.image0} info_content={content.info_content0} />
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="research_wrapper">
+              <Headerfirst id="2" date={content.date1} url={content.url1} show_pdf={false} />
               <Headermain heading={content.heading1} image={content.image1} info_content={content.info_content1} />
             </div>
           </div>
-          <div className="col-sm-6">
-            <div className="research_wrapper">
-              <Headerfirst id="1" date={content.date2} url={content.url2} show_pdf={false} />
-              <Headermain heading={content.heading2} image={content.image2} info_content={content.info_content2} />
-            </div>
-          </div>
         </div>
+
         <div className="row">
           <div className="col-sm-6">
             <div className="research_wrapper">
-              <Headerfirst id="3" date="27.02.2011." url="" />
-              <Headermain
-                heading={'Kidney disease'}
-                image={'../../pics/organs/kidney.jpg'}
-                info_content=" laksjflkasjf aklsfj alfkajfl kasjflakf jalkfj alkaj flkas faklčsfj alksfjalskfjasl čfajslkf aslkfasj flakj faklfj alfkaj flkas fjlakakljfdkla flasjf askfjdsal kfjafklč ajkčajs flkčsjf aklsčfajs fkaf asdlkčfj asflčkajsflčksdajfas čfjka asklčfajsfkl čafjsl čkasfjalčkfjdlčkfj aflčkajsf alčskfj asslčfkajs flksajflakčs fjakdsfj ads "
-              />
+              <Headerfirst id="3" date={content.date2} url={content.url2} show_pdf={false} />
+              <Headermain heading={content.heading2} image={content.image2} info_content={content.info_content2} />
             </div>
           </div>
           <div className="col-sm-6">
             <div className="research_wrapper">
-              <Headerfirst id="4" date="27.02.2011." url="" />
-              <Headermain heading={'Heart disease'} image={'../../pics/organs/heart.png'} info_content="" />
+              <Headerfirst id="4" date={content.date3} url={content.url3} show_pdf={false} />
+              <Headermain heading={content.heading3} image={content.image3} info_content={content.info_content3} />
             </div>
           </div>
         </div>
 
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="research_wrapper">
+              <Headerfirst id="5" date={content.date4} url={content.url4} show_pdf={false} />
+              <Headermain heading={content.heading4} image={content.image4} info_content={content.info_content4} />
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="research_wrapper">
+              <Headerfirst id="6" date={content.date5} url={content.url5} show_pdf={false} />
+              <Headermain heading={content.heading5} image={content.image5} info_content={content.info_content5} />
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="research_wrapper">
+              <Headerfirst id="7" date={content.date6} url={content.url6} show_pdf={false} />
+              <Headermain heading={content.heading6} image={content.image6} info_content={content.info_content6} />
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="research_wrapper">
+              <Headerfirst id="8" date={content.date7} url={content.url7} show_pdf={false} />
+              <Headermain heading={content.heading7} image={content.image7} info_content={content.info_content7} />
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="research_wrapper">
+              <Headerfirst id="9" date={content.date8} url={content.url8} show_pdf={false} />
+              <Headermain heading={content.heading8} image={content.image8} info_content={content.info_content8} />
+            </div>
+          </div>
+          <div className="col-sm-6">
+            <div className="research_wrapper">
+              <Headerfirst id="10" date={content.date9} url={content.url9} show_pdf={false} />
+              <Headermain heading={content.heading9} image={content.image9} info_content={content.info_content9} />
+            </div>
+          </div>
+        </div>
         <Social shown={true} />
         <Navfooter />
       </div>
