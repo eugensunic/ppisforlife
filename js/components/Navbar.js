c4a459808997b95f9ac2d530fc31173875a3ecaf
Eugen Sunic <eugen.sunic@extern.consorsbank.de>
2018-06-15 22:40:00 +0200

    removed multiple push error for main navigation header

--- js/components/Navbar.js
+++ js/components/Navbar.js
@@ -20,63 +20,100 @@ export default class Navbar extends React.Component {
         <div className="nav_center">
           <Route
             render={({ history }) => (
-              <span className="nav_element" onClick={() => history.push('/')}>
+              <span
+                className="nav_element"
+                onClick={() => {
+                  history.location.pathname !== '/' ? history.push('/') : '';
+                }}>
                 home
               </span>
             )}
           />
           <Route
             render={({ history }) => (
-              <span className="nav_element" onClick={() => history.push('/posts')}>
+              <span
+                className="nav_element"
+                onClick={() => {
+                  history.location.pathname !== '/posts' ? history.push('/posts') : '';
+                }}>
                 posts
               </span>
             )}
           />
           <Route
             render={({ history }) => (
-              <span className="nav_element" onClick={() => history.push('/statistics')}>
+              <span
+                className="nav_element"
+                onClick={() => {
+                  history.location.pathname !== '/statistics' ? history.push('/statistics') : '';
+                }}>
                 statistics
               </span>
             )}
           />
           <Route
             render={({ history }) => (
-              <span className="nav_element" onClick={() => history.push('/research')}>
+              <span
+                className="nav_element"
+                onClick={() => {
+                  history.location.pathname !== '/research' ? history.push('/research') : '';
+                }}>
                 research
               </span>
             )}
           />
           <Route
             render={({ history }) => (
-              <span className="nav_element" onClick={() => history.push('/form')}>
+              <span
+                className="nav_element"
+                onClick={() => {
+                  history.location.pathname !== '/form' ? history.push('/form') : '';
+                }}>
                 form
               </span>
             )}
           />
           <Route
             render={({ history }) => (
-              <span className="nav_element" onClick={() => history.push('/tips')}>
+              <span
+                className="nav_element"
+                onClick={() => {
+                  history.location.pathname !== '/tips' ? history.push('/tips') : '';
+                }}>
                 tips
               </span>
             )}
           />
           <Route
             render={({ history }) => (
-              <span className="nav_element" onClick={() => history.push('/faq')}>
+              <span
+                className="nav_element"
+                onClick={() => {
+                  history.location.pathname !== '/faq' ? history.push('/faq') : '';
+                }}>
                 faq
               </span>
             )}
           />
           <Route
             render={({ history }) => (
-              <span className="nav_element" onClick={() => history.push('/about')}>
+              <span
+                className="nav_element"
+                onClick={() => {
+                  history.location.pathname !== '/about' ? history.push('/about') : '';
+                }}>
                 about
               </span>
             )}
           />
           <Route
             render={({ history }) => (
-              <span className="nav_element" style={{ marginLeft: 15 }} onClick={() => history.push('/overview')}>
+              <span
+                className="nav_element"
+                style={{ marginLeft: 15 }}
+                onClick={() => {
+                  history.location.pathname !== '/overview' ? history.push('/overview') : '';
+                }}>
                 form overview
               </span>
             )}
